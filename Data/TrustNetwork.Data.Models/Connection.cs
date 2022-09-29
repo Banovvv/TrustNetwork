cf7f9990233e477046e64e502eae584f30c7db43
Ivan Gechev <ivan_banov@abv.bg>
2022-09-29 15:47:38 +0300

    More model work

--- Data/TrustNetwork.Data.Models/Connection.cs
+++ Data/TrustNetwork.Data.Models/Connection.cs
@@ -3,7 +3,13 @@
     public class Connection
     {
         public int Id { get; set; }
-        public string PersonID { get; set; }
+
+        public int PersonID { get; set; }
+        public Person Person { get; set; }
+
+        public int ConnectedPersonID { get; set; }
+        public Person ConnectedPerson { get; set; }
+
         public int TrustLevel { get; set; }
     }
 }
--- Data/TrustNetwork.Data.Models/Person.cs
+++ Data/TrustNetwork.Data.Models/Person.cs
@@ -2,6 +2,12 @@
 {
     public class Person
     {
+        public Person()
+        {
+            Topics = new HashSet<Topic>();
+            Connections = new HashSet<Connection>();
+        }
+
         public string ID { get; set; }
         public ICollection<Topic> Topics { get; set; }
         public ICollection<Connection> Connections { get; set; }
