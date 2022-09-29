d7dfcffafd416135cb0eaa09f5ee0d3f7e583650
Ivan Gechev <ivan_banov@abv.bg>
2022-09-29 15:51:23 +0300

    DbContext

--- Data/TrustNetwork.Data/ApplicationDataContext.cs
+++ Data/TrustNetwork.Data/ApplicationDataContext.cs
@@ -1,7 +1,29 @@
-﻿namespace TrustNetwork.Data
-{
-    public class ApplicationDataContext
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace TrustNetwork.Data
+{
+    public class ApplicationDataContext : DbContext
+    {
+        public ApplicationDataContext()
+        {
+        }
+
+        public ApplicationDataContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer();
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
