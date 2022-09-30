db531b464f7a70c48be7affb92c2a62e8ba14486
Ivan Gechev <ivan_banov@abv.bg>
2022-09-30 07:55:47 +0300

    PeopleController

---
+++ Web/TrustedNetwork.Web/Controllers/PeopleController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TrustedNetwork.Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PeopleController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<int> Get()
+        {
+            return new int[] { 0, 1, 2 };
+        }
+    }
+}
